c187ef13b5f9619b631f42134db92a22b49f3f2e
Logan <lmyers@pretalen.com>
2017-12-29 02:26:17 -0500

    Removed Commeted out code. Removed creep speech when all containers are filled.

--- role.harvester.js
+++ role.harvester.js
@@ -72,7 +72,7 @@ var roleHarvester = {
                     }
                 }
                 else {
-                    creep.say('All Storage full...');
+                    //creep.say('All Storage full...');
                 }
             }
 	    }
@@ -90,72 +90,6 @@ var roleHarvester = {
 	            creep.say('🔄 Storing Energy');
             }
 	    }
-
-
-	    //if(creep.carry.energy < creep.carryCapacity) {
-        //    var sources = creep.room.find(FIND_SOURCES);
-        //    var harvest_source_num = 1;
-        //    if(creep.memory.role == 'builder') harvest_source_num = 0;
-        //    if(creep.harvest(sources[harvest_source_num]) == ERR_NOT_IN_RANGE) {
-        //        creep.moveTo(sources[harvest_source_num], {visualizePathStyle: {stroke: '#ffaa00'}});
-        //    }
-        //}
-        //else {
-
-        //    let extension = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        //        filter: s => s.structureType == STRUCTURE_EXTENSION && s.energy != 50
-        //    });
-        //    let spawn = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        //        filter: s => s.structureType == STRUCTURE_SPAWN && s.energy < 300
-        //    });
-        //    let container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        //        filter: s => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] < s.storeCapacity
-        //    });
-        //    let storage_target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        //        filter: s => s.structureType == STRUCTURE_STORAGE && s.store[RESOURCE_ENERGY] < s.storeCapacity
-        //    });
-        //    if(extension) {
-        //        if(creep.transfer(extension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        //            creep.moveTo(extension, {visualizePathStyle: {stroke: '#ffffff'}});
-        //        }
-        //    }
-        //    else if(spawn) {
-        //        if(creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        //            creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffffff'}});
-        //        }
-        //    }
-        //    else if(container){
-        //        if(container!=undefined){
-        //            if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        //                creep.moveTo(container, {visualizePathStyle: {stroke: '#ffff00'}});
-        //            }
-        //        }
-        //    }
-        //    else if(storage_target){
-        //        if(storage_target!=undefined){
-        //            if(creep.transfer(storage_target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        //                creep.moveTo(storage_target, {visualizePathStyle: {stroke: '#ffff00'}});
-        //            }
-        //        }
-        //    }
-        //    else {
-        //        var targets = creep.room.find(FIND_STRUCTURES, {
-        //            filter: (structure) => {
-        //                return (structure.structureType == STRUCTURE_EXTENSION ||
-        //                    structure.structureType == STRUCTURE_SPAWN ||
-        //                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-        //            }
-        //        });
-        //        if(targets.length > 0) {
-        //            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        //                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-        //            }
-        //        }
-        //        else {
-        //            creep.say('All Storage full...');
-        //        }
-        //    }
-        //}
 	}
 };
 
