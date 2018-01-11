05b8aa0a1174e46cab7dd29d8e99099a74e1f380
logan <lmyers@pretalen.com>
2018-01-11 17:09:43 -0500

    added some additional files

---
+++ settings.flag_handler.js
@@ -0,0 +1,21 @@
+/* flag objects/functions
+.pos
+.room
+.color
+.memory
+.name
+.secondaryColor
+
+.remove()
+.setColor(color, [secondaryColor])
+.setPosition(x,y)(pos)
+*/
+
+module.exports = {
+  flag_evaluator(flag){
+    
+  },
+  delete_flag(flag){
+
+  }
+}
---
+++ settings.room_expansion.js
@@ -0,0 +1,5 @@
+module.exports = {
+  evaluate_room(room_id){
+    
+  }
+}
